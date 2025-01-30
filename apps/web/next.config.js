da58ebcb870c24c58ebf8d1a9dc776326b64b93a
Jonathan Ruffles <icondesk1@gmail.com>
2025-01-30 14:49:48 +0000

    add files

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,10 +1,4 @@
-const path = require("path");
-
+/** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-  transpilePackages: ["@repo/ui"],
   output: "standalone",
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, "../../"),
-  },
 };
